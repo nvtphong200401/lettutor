ea7a97a9af7e458e7dbad00a4358f93d9d31f753
nvtphong-Executionlab <phong.nguyen@executionlab.asia>
2023-03-15 17:12:02 +0700

    refactor code

---
+++ lib/presentation/stream/preview_widget.dart
@@ -0,0 +1,151 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class PreviewWidget extends HookWidget {
+  const PreviewWidget(
+    this.controller, {
+    required this.micNotifier,
+    required this.cameraNotifier,
+    required this.raiseHandNotifier,
+    Key? key,
+  }) : super(key: key);
+
+  final CameraController controller;
+  final ValueNotifier<bool> micNotifier;
+  final ValueNotifier<bool> cameraNotifier;
+  final ValueNotifier<bool> raiseHandNotifier;
+
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width / 4;
+    return Container(
+      width: width,
+      height: 9 * width / 16,
+      margin: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(4.0),
+      ),
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(4.0),
+              child: CameraPreview(controller),
+            ),
+          ),
+          Positioned.fill(
+            child: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4.0),
+                color: Colors.black.withOpacity(0.5),
+              ),
+            ),
+          ),
+          const Align(
+            alignment: Alignment.bottomLeft,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 4.0, left: 2.0),
+              child: Icon(
+                Icons.more_vert,
+                color: Colors.white,
+                size: 12.0,
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 4.0, left: 2.0),
+              child: Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(2.0),
+                    margin: const EdgeInsets.all(2.0),
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white,
+                    ),
+                    child: Container(
+                      padding: const EdgeInsets.all(4.0),
+                      decoration: const BoxDecoration(shape: BoxShape.circle, color: Colors.green),
+                      child: const Icon(
+                        Icons.network_cell,
+                        color: Colors.white,
+                        size: 8.0,
+                      ),
+                    ),
+                  ),
+                  HookBuilder(builder: (context) {
+                    final raiseHand = useValueListenable(raiseHandNotifier);
+                    return Visibility(
+                      visible: raiseHand,
+                      child: Container(
+                        padding: const EdgeInsets.all(2.0),
+                        margin: const EdgeInsets.all(2.0),
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        ),
+                        child: Container(
+                          padding: const EdgeInsets.all(4.0),
+                          decoration:
+                              const BoxDecoration(shape: BoxShape.circle, color: Colors.orange),
+                          child: const Icon(
+                            Icons.back_hand,
+                            color: Colors.white,
+                            size: 8.0,
+                          ),
+                        ),
+                      ),
+                    );
+                  }),
+                ],
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 4.0, right: 4.0),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  HookBuilder(builder: (context) {
+                    final mic = useValueListenable(micNotifier);
+                    return Visibility(
+                      visible: !mic,
+                      child: const Icon(
+                        Icons.mic_off,
+                        color: Colors.white,
+                        size: 12.0,
+                      ),
+                    );
+                  }),
+                  const SizedBox(width: 4.0),
+                  HookBuilder(builder: (context) {
+                    final camera = useValueListenable(cameraNotifier);
+                    return Visibility(
+                      visible: !camera,
+                      child: const Icon(
+                        Icons.videocam_off_outlined,
+                        color: Colors.white,
+                        size: 12.0,
+                      ),
+                    );
+                  }),
+                  const SizedBox(width: 4.0),
+                  const Icon(
+                    Icons.star_border,
+                    color: Colors.white,
+                    size: 12.0,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/presentation/teacher/teacher_card_item.dart
+++ lib/presentation/teacher/teacher_card_item.dart
@@ -17,69 +17,64 @@ class TeacherCardItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final info = TeacherModel.init();
-    return InkWell(
-      onTap: () => context.router.push(const TeacherDetailRoute()),
-      // Navigator.of(context)
-      //     .push(MaterialPageRoute(builder: (context) => const TeacherDetailScreen())),
-      child: Card(
-        shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-            side: const BorderSide(color: ColorName.activeTag)),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TeacherInfo(
-                name: info.name,
-                avatar: info.avatar,
-                isFavorite: info.isFavorite,
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              Wrap(
-                children: const [
-                  CommonTag(title: 'English for kids'),
-                  CommonTag(title: 'IELTS'),
-                ],
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              ReadMoreText(info.bio),
-              const SizedBox(
-                height: 10,
-              ),
-              Align(
-                alignment: Alignment.centerRight,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: CommonButtonStyle.primaryButtonStyle.customCopyWith(
-                      capsuleShape: true,
-                      backgroundColor: ColorName.background,
-                      borderColor: ColorName.textButton),
-                  child: SizedBox(
-                    width: 80,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: const [
-                        Icon(
-                          Icons.calendar_month_sharp,
-                          color: ColorName.textButton,
-                        ),
-                        Text(
-                          'Book',
-                          style: TextStyle(color: ColorName.textButton),
-                        )
-                      ],
-                    ),
+    return Card(
+      shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+          side: const BorderSide(color: ColorName.activeTag)),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TeacherInfo(
+              name: info.name,
+              avatar: info.avatar,
+              isFavorite: info.isFavorite,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Wrap(
+              children: const [
+                CommonTag(title: 'English for kids'),
+                CommonTag(title: 'IELTS'),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ReadMoreText(info.bio),
+            const SizedBox(
+              height: 10,
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: ElevatedButton(
+                onPressed: () => context.router.push(const TeacherDetailRoute()),
+                style: CommonButtonStyle.primaryButtonStyle.customCopyWith(
+                    capsuleShape: true,
+                    backgroundColor: ColorName.background,
+                    borderColor: ColorName.textButton),
+                child: SizedBox(
+                  width: 80,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: const [
+                      Icon(
+                        Icons.calendar_month_sharp,
+                        color: ColorName.textButton,
+                      ),
+                      Text(
+                        'Book',
+                        style: TextStyle(color: ColorName.textButton),
+                      )
+                    ],
                   ),
                 ),
-              )
-            ],
-          ),
+              ),
+            )
+          ],
         ),
       ),
     );
