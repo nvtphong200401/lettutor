348c8b5f85b81d68a721e88180101b02b6cf127e
nvtphong200401 <hackkerguy11@gmail.com>
2023-03-17 21:08:06 +0700

    update pdf viewer

--- lib/presentation/courses/detail/course_detail_screen.dart
+++ lib/presentation/courses/detail/course_detail_screen.dart
@@ -1,36 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:lettutor/gen/colors.gen.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
 class CourseDetailScreen extends HookWidget {
-  const CourseDetailScreen({super.key, required this.title, required this.courseUrl});
+  const CourseDetailScreen(
+      {super.key, required this.title, required this.courseUrl});
   final String title;
   final String courseUrl;
 
   @override
   Widget build(BuildContext context) {
-    final webViewController = useMemoized(() => WebViewController());
-    useEffect(() {
-      webViewController.setJavaScriptMode(JavaScriptMode.unrestricted);
-      webViewController.setNavigationDelegate(NavigationDelegate(
-        onNavigationRequest: (request) {
-          return NavigationDecision.navigate;
-        },
-      ));
-      webViewController.loadRequest(Uri.parse(courseUrl));
-      return null;
-    }, [webViewController]);
+    // final webViewController = useMemoized(() => WebViewController());
+    // useEffect(() {
+    //   webViewController.setJavaScriptMode(JavaScriptMode.unrestricted);
+    //   webViewController.setNavigationDelegate(NavigationDelegate(
+    //     onNavigationRequest: (request) {
+    //       return NavigationDecision.navigate;
+    //     },
+    //   ));
+    //   webViewController.loadRequest(Uri.parse(courseUrl));
+    //   return null;
+    // }, [webViewController]);
     // return SfPdfViewer.network(
     //     'https://cdn.syncfusion.com/content/PDFViewer/flutter-succinctly.pdf');
     return Scaffold(
-      body: WebViewWidget(controller: webViewController),
+      body: SfPdfViewer.network(courseUrl),
       appBar: AppBar(
         foregroundColor: ColorName.primary,
         title: Text(
           title,
-          style:
-              const TextStyle(fontSize: 18, fontWeight: FontWeight.w600, color: ColorName.primary),
+          style: const TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w600,
+              color: ColorName.primary),
         ),
       ),
     );
